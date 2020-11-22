d9e41aa9ee113dee860048fd533881657b1b04f8
JonnyLinja <jonny.linja@gmail.com>
2020-11-22 02:21:35 -0500

    Functional for ios dev mode at least. This appears to be the approach I'm going with, just need to sanity check release and android

--- apps/MyApp/metro.config.js
+++ apps/MyApp/metro.config.js
@@ -6,29 +6,14 @@
  */
 
 const path = require('path');
-const blacklist = require('metro-config/src/defaults/blacklist');
 
-// from https://blog.g2i.co/how-to-set-up-a-monorepo-with-react-native-you-i-and-yarn-workspaces-3e83c3f08cf1
-// ignoring the blacklist as I don't have a web folder with a build directory here
 const watchFolders = [
-  path.resolve(__dirname, '../../node_modules'),
-  path.resolve(__dirname, '../../shared'),
-  // path.resolve(__dirname, '../../shared/node_modules'), // not sure this has any effect, random attempt
+  path.resolve(__dirname, '../../node_modules'), // hoisted node modules
+  path.resolve(__dirname, '../../shared'), // shared source code
 ];
 
-// from https://medium.com/@dushyant_db/how-to-import-files-from-outside-of-root-directory-with-react-native-metro-bundler-18207a348427
-// this was having issues however, so ended up using the above watch folders instead
-// const extraNodeModules = {
-//   'shared': path.resolve(__dirname + '/../../'),
-// };
-// const watchFolders = [
-//   path.resolve(__dirname + '/../../shared')
-// ];
-
-// from https://medium.com/@huntie/a-concise-guide-to-configuring-react-native-with-yarn-workspaces-d7efa71b6906
-// and https://gist.github.com/huntie/85ea491763b444bfa1bdc8e997fc2765
+// native modules
 const extraNodeModules = {
-  // Resolve all react-native module imports to the locally-installed version
   'react-native': path.resolve(__dirname, 'node_modules', 'react-native'),
   'react-native-device-info': path.resolve(__dirname, 'node_modules', 'react-native-device-info'),
 };
@@ -43,10 +28,6 @@ module.exports = {
     }),
   },
   resolver: {
-    blacklistRE: blacklist([
-      // /^((?!shared).)+[\/\\]node_modules[/\\]react-native[/\\].*/,
-      // /shared[\/\\]node_modules[/\\]react-native-device-info[/\\].*/,
-    ]),
     extraNodeModules
   },
   watchFolders,
--- shared/utilities/hello/index.ts
+++ shared/utilities/hello/index.ts
@@ -2,5 +2,5 @@ import * as math from 'mathjs';
 import DeviceInfo from 'react-native-device-info';
 
 export default function hello(x: number) {
-    return `${DeviceInfo.getApplicationName()}: ${math.evaluate('x*x', {x:x})}`;
+    return `${DeviceInfo.getUniqueId()}: ${math.evaluate('x*x', {x:x})}`;
 }
